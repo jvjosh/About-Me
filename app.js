5bcb1417e5fdb6925f11b53ab749bd5e2ba0f911
jvjosh <jvjosh@gmail.com>
2017-01-03 20:00:09 -0800

    work complete

---
+++ app.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var question1 = prompt('Are you liking Codefellows so far?');
+console.log('user is prompted to ask a yes or no question');
+
+if (question1.toLowerCase() === 'yes') {
+  alert('Keep up the good work!');
+}
+else {
+  alert('Sorry, Hope it gets better!');
+}
+
+var question2 = prompt('Do you live in Washington?');
+console.log('user was asked if he/she lives in Washington');
+
+if (question2.toLowerCase() === 'yes') {
+  alert('You might have webbed feet! LOL');
+}
+else {
+  alert('You are missing out on the beauty of only Rainforest in the USA!');
+}
+
+var question3 = prompt('Do you like Pizza?');
+console.log('user is prompted to ask a yes or no question about pizza');
+
+if (question3.toLowerCase() === 'yes') {
+  alert('Pizza is the  BEST!');
+}
+else {
+  alert('Too bad for you.');
+}
+
+var question4 = prompt('Have you traveled around the world?');
+console.log('user has or has not traveled the world');
+
+if (question4.toLowerCase() === 'yes') {
+  alert('Wow!');
+}
+else {
+  alert('I\'m just a chouch potato.');
+}
+var question5 = prompt('Do you have a pet?');
+console.log('user is asked if they have a pet');
+
+if (question5.toLowerCase() === 'yes') {
+  alert('Having a pet helps you to live longer.');
+}
+else {
+  alert('You should get a pet!');
+}
